9f17b24df20b32298a5099a08131236d7ee59039
Antoine Nzeyi <anthonzeyi@gmail.com>
2024-01-28 20:31:20 -0500

    Removed checkpoints dir creation from eval

--- scripts/classification_regression/eval.sh
+++ scripts/classification_regression/eval.sh
@@ -3,8 +3,6 @@ export KINLP_HOME=/opt/KINLP
 export PATH=$PATH:$KINLP_HOME:$KINLP_HOME/bin/$UBUNTU_VERSION
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$KINLP_HOME/lib/$UBUNTU_VERSION
 
-mkdir -p /root/DeepKIN/scripts/classification_regression/checkpoints
-
 python3 semeval2024_str_eval.py \
   -g 1 \
   --eval-dataset="/root/Semantic_Relatedness_SemEval2024/Track A/kin/kin_test.csv" \
